c8d50e0500d9013572d68718a4b20d4446290a06
m0ssc0de <hi.paul.q@gmail.com>
2020-04-28 16:54:14 +0800

    add server

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 #[macro_use]
 extern crate clap;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
-fn main() {
+async fn index() -> impl Responder {
+     HttpResponse::Ok().body("Hello world!")
+}
+
+async fn index2() -> impl Responder {
+     HttpResponse::Ok().body("Hello world again!")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
      let matches = clap_app!(pharmacy =>
           (version: "0.1")
           (author: "m0ssc0de <hi.paul.q@gmail.com>")
@@ -23,4 +33,13 @@ fn main() {
           let port = matches.value_of("PORT").unwrap_or(":8080");
           println!("Printing specify port to listen {}", port);
      }
+
+     HttpServer::new(|| {
+          App::new()
+               .route("/", web::get().to(index))
+               .route("/again", web::get().to(index2))
+     })
+     .bind("127.0.0.1:8088")?
+     .run()
+     .await
 }
