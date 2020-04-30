c0403a7635f6e7c238c2000f8a3ebeae16d76bf0
m0ssc0de <hi.paul.q@gmail.com>
2020-04-30 17:22:07 +0800

    update env shell

--- dev-env/install-env.sh
+++ dev-env/install-env.sh
@@ -4,10 +4,12 @@ set -eux
 
 yum -y install  https://centos7.iuscommunity.org/ius-release.rpm
 
-yum install -y gcc cmake sqlite3-devel git2u-all
+yum install -y gcc cmake sqlite-devel git2u-all
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 source $HOME/.cargo/env
 
 cargo install cargo-web
+
+cargo install diesel_cli --no-default-features --features sqlite
