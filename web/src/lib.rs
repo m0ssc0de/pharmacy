469f57766a7650dfddae9afda45bf0ac8b35c008
m0ssc0de <hi.paul.q@gmail.com>
2020-05-15 16:31:16 +0800

    html ok

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -40,24 +40,37 @@ impl Component for Model {
                 <div
                     class="uk-card uk-card-default uk-card-body"
                     style="margin-bottom: 200px; z-index: 980;"
-                    uk-sticky="top: 100; animation: uk-animation-slide-top; bottom: #sticky-on-scroll-up"
+                    uk-sticky="animation: uk-animation-slide-top; bottom: #sticky-on-scroll-up"
                 >
                     <h1 class="uk-heading-small uk-text-lead"> {"Tomato Workstation"} </h1>
                 </div>
                 <div class="uk-section">
-                    <div class="uk-container uk-container-small">
-
+                    <div class="uk-container uk-container-small" uk-sticky="uk-flex-top">
+                        <div>
+                            <div class="uk-card uk-card-default uk-card-small">
+                                <div class="uk-text-center uk-card-body">
+                                    {"Do you want a tomato after 1 hour work?"}<br /> {"Let's get details."}
+                                    <br />
+                                </div>
+                            </div>
+                        </div>
+                        <h1 class="uk-heading-divider"></h1>
                         <div class="uk-card uk-card-default uk-card-hover uk-card-body">
                             <div class="uk-grid uk-child-width-1-1">
                                 <div>
+                                    <div class="uk-text-center uk-card-body uk-margin-small-top">
+                                        {"Input your Access ID and Key."}
+                                    </div>
+                                </div>
+                                <div>
                                     <div class="uk-grid uk-child-width-1-2">
-                                        <label class="uk-text-large">{"test1"}</label>
+                                        <label class="uk-text-large">{"Access ID"}</label>
                                         <input class="uk-input uk-width-1-2" type="text" placeholder="as;ldkf;lk" />
                                     </div>
                                 </div>
                                 <div>
                                     <div class="uk-grid uk-child-width-1-2">
-                                        <label class="uk-text-large">{"test2"}</label>
+                                        <label class="uk-text-large">{"Access Key"}</label>
                                         <input class="uk-input uk-width-1-2" type="text" placeholder="as;ldkf;lk" />
                                     </div>
                                 </div>
@@ -76,7 +89,10 @@ impl Component for Model {
                                 <label class="uk-text-large">{"Good"}</label>
                                 <input class="uk-range" type="range" value="2" min="1" max="3" step="1" />
                             </div>
-                            <button class="uk-button uk-button-primary" onclick=self.link.callback(|_| Msg::CreatOne)>{ "+1" }</button>
+                            <br />
+                            // <div class="uk-grid">
+                                <button class="uk-container-center uk-button uk-button-primary" onclick=self.link.callback(|_| Msg::CreatOne)>{ "+1" }</button>
+                            // </div>
                         </div>
                     </div>
                 </div>
