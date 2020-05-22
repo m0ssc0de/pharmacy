27ed5d330f2a659d5d877805fef31bb16bb920c7
m0ssc0de <hi.paul.q@gmail.com>
2020-05-22 17:13:24 +0800

    add websocket

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,16 +1,21 @@
 #![recursion_limit = "512"]
+use failure::Error;
 use wasm_bindgen::prelude::*;
 use yew::prelude::*;
 use yew::services;
+use yew::services::websocket::{WebSocketService, WebSocketStatus, WebSocketTask};
 use yew::InputData;
 
-struct Model {
+pub struct Model {
     link: ComponentLink<Self>,
+    ws: Option<WebSocketTask>,
+    wss: WebSocketService,
     value: i64,
     access_id: String,
     access_key: String,
     instance_level: i64,
     level_name: String,
+    server_data: String,
 }
 
 impl Model {
@@ -19,10 +24,12 @@ impl Model {
     }
 }
 
-enum Msg {
+pub enum Msg {
     AddOne,
     CreatOne,
     Level(String),
+    Connect,
+    Received(Result<String, Error>), // data received from server
 }
 
 impl Component for Model {
@@ -31,11 +38,14 @@ impl Component for Model {
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         Self {
             link,
+            ws: None,
+            wss: WebSocketService::new(),
             value: 0,
             access_id: String::from(""),
             access_key: String::from(""),
             instance_level: 2,
             level_name: String::from("Better"),
+            server_data: "server data".to_owned(),
         }
     }
 
@@ -44,6 +54,33 @@ impl Component for Model {
         let mut ys = services::console::ConsoleService::new();
 
         match msg {
+            Msg::Received(Ok(s)) => {
+                self.server_data.push_str(&format!("{}\n", &s));
+            }
+            Msg::Received(Err(s)) => {
+                self.server_data.push_str(&format!(
+                    "Error when reading data from server: {}\n",
+                    &s.to_string()
+                ));
+            }
+            Msg::Connect => {
+                // self.console.log("Connecting");
+                // let cbout = self.link.send_back(|Json(data)| Msg::Received(data));
+                // let cbnot = self.link.send_back(|input| {
+                //     ConsoleService::new().log(&format!("Notification: {:?}", input));
+                //     match input {
+                //         WebSocketStatus::Closed | WebSocketStatus::Error => Msg::Disconnected,
+                //         _ => Msg::Ignore,
+                //     }
+                // });
+                // if self.ws.is_none() {
+                //     let task = self
+                //         .wss
+                //         .connect("ws://127.0.0.1:8080/ws/", cbout, cbnot.into());
+                //     self.ws = Some(task);
+                // }
+                // true
+            }
             Msg::AddOne => {
                 ys.log("AddOne");
                 self.value += 1;
