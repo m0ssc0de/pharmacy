2026aa7d3d1c6c24ac23c357b3d4273b1aac8041
m0ssc0de <hi.paul.q@gmail.com>
2020-05-22 16:28:49 +0800

    move back cargo-web

--- web/live-code-server.sh
+++
@@ -1,5 +0,0 @@
-inotifywait -m ./ -e close_write  -e moved_to |
-    while read path action file; do
-        echo "The file '$file' appeared in directory '$path' via '$action'"
-        wasm-pack build --target web --out-name wasm --out-dir ./static && miniserve ./static --index index.html
-    done
